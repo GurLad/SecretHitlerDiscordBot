7605bc9e28f2de43c2a277d580332ad015b63196
Guy West <gywst@users.noreply.github.com>
2021-01-14 20:24:59 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,27 @@
+const say = require('say');
+
+let voices = [
+  { voice: 'Microsoft David Desktop', text: 'No people recognize me by my voice.' },
+  { voice: 'Microsoft Zira Desktop', text: 'No people recognize me by my voice.' }
+]
+
+sayIt(0)
+
+function sayIt (voice) {
+  let v = voices[voice]
+  console.log(v.voice)
+
+  say.speak(v.text, v.voice, 1, (err) => {
+    voice++
+
+    if (err) {
+      console.error(err)
+    }
+
+    if (voice >= voices.length) {
+      process.exit(0)
+    }
+
+    sayIt(voice)
+  })
+}
