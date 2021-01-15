003c2df81748cdf956c2d6d3fadfd2f93a319cd8
Guy West <gywst@users.noreply.github.com>
2021-01-15 13:33:59 +0200

    Update platform.js
    platform test code

--- tests/platform.js
+++ tests/platform.js
@@ -4,10 +4,27 @@
 // https://nodejs.org/api/process.html#process_process_platform
 // aix, darwin, freebsd, linux, openbsd, sunos, win32
 
-if (process.platform === "win32") {
-    console.log("Microsoft Windows");
-} else if (process.platform === "darwin") {
-    console.log("Apple Mac OS X");
-} else if (process.platform === "linux") {
-    console.log("Linux");
+// if (process.platform === "win32") {
+//     console.log("Microsoft Windows");
+// } else if (process.platform === "darwin") {
+//     console.log("Apple Mac OS X");
+// } else if (process.platform === "linux") {
+//     console.log("Linux");
+// }
+
+var processPlatform = process.platform
+
+switch (processPlatform) {
+    case "win32":
+        console.log("Microsoft Windows");
+        break;
+    case "darwin":
+        console.log("Apple Mac OS X");
+        break;
+    case "linux":
+        console.log("Linux");
+        break;
+    default:
+        console.log("other environment: " + processPlatform);
+        break;
 }
